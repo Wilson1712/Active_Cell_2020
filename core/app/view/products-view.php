b5b9bddbf5b7e1c1cbde20196be6faa7403e8917
Camila2017 <57103555+Camila2017@users.noreply.github.com>
2020-03-24 14:08:13 -0500

    products

--- core/app/view/products-view.php
+++ core/app/view/products-view.php
@@ -1,3 +1,8 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/alertifyjs@1.11.0/build/css/alertify.min.css" rel="stylesheet"/>
+<script src="https://cdn.jsdelivr.net/npm/alertifyjs@1.11.0/build/alertify.min.js"></script>
 <div class="row">
 	<div class="col-md-12">
 <div class="btn-group  pull-right">
